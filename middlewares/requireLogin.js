4ebbf596c429145cec0da9c8fb3fa3405c21086a
Amer Kadić <akadic5@etf.unsa.ba>
2019-06-20 15:58:04 +0200

    Added billing and clients

---
+++ middlewares/requireLogin.js
@@ -0,0 +1,8 @@
+module.export = (req, res, next) => {
+	if(!req.user){
+ 			return res.status(401).send({error: 'You must login'});
+ 		}
+
+ 		next();
+
+};
--- services/passport.js
+++ services/passport.js
@@ -1,34 +1,37 @@
-const GoogleStrategy = require ('passport-google-oauth20').Strategy;
-const passport = require ('passport');
-const mongoose = require('mongoose');
-const keys = require('../config/keys.js');
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const passport = require("passport");
+const mongoose = require("mongoose");
+const keys = require("../config/keys.js");
 
-const User = mongoose.model('users');
+const User = mongoose.model("users");
 passport.serializeUser((user, done) => {
-	done(null,user.id);
+	done(null, user.id);
 });
 
 passport.deserializeUser((id, done) => {
 	User.findById(id).then(user => {
-		done(null,user);
+		done(null, user);
 	});
 });
 
-passport.use(new GoogleStrategy({
-	clientID: keys.googleClientID,
-	clientSecret: keys.googleClientSecret,
-	callbackURL: '/auth/google/callback' ,
-	proxy : true
-}, (accessToken, refreshToken, profile, done) =>{
-
-    User.findOne({googleId: profile.id}).then((existingUser) =>
-	{
-		if(existingUser) {
-			done(null,existingUser);
+passport.use(
+	new GoogleStrategy(
+		{
+			clientID: keys.googleClientID,
+			clientSecret: keys.googleClientSecret,
+			callbackURL: "/auth/google/callback",
+			proxy: true
+		},
+		(accessToken, refreshToken, profile, done) => {
+			User.findOne({ googleId: profile.id }).then(existingUser => {
+				if (existingUser) {
+					done(null, existingUser);
+				} else {
+					new User({ googleId: profile.id })
+						.save()
+						.then(user => done(null, user));
+				}
+			});
 		}
-		else{
-				new User ({googleId: profile.id}).save().then(user => done(null,user));
-		}
-	});
-}
-));
+	)
+);
